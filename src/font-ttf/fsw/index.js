d341bc2dcaf730ff694fb2fe7f825051dd48ac46
Michael Cooper <1991467+michael-n-cooper@users.noreply.github.com>
2026-03-25 11:22:36 -0400

    doc for main module

--- src/font-ttf/fsw/index.js
+++ src/font-ttf/fsw/index.js
@@ -1,13 +1,20 @@
 /**
- * FSW
+ * This is a set of proposed additional features for the {@link https://www.sutton-signwriting.io/font-ttf/ Sutton Signwriting Javascript library}. For prototyping and demo purposes this is implemented only in the font-ttf module, and if accepted would need porting to the other modules. Some of the functions might be more appropriate for the core module but are here for now to keep it simple.
+ * 
+ * Types of features:
+ * * Symbol state functions to determine handedness, plane, and palm orientation;
+ * * Symbol modification functions that alter these states, and an extended left-right mirror function handling additional ranges;
+ * * Generate semantic descriptions of symbols and in turn for entire signs;
+ * * Flip signs between left- and right-handed signs, and between signer and viewer perspective;
+ * * Generate a reasonable temporal sequence for a sign.
  * @module ext/ttf/fsw
  */
 
-import { signDescribe } from "./fsw-sign-description.js";
-import { signFlipX, signFlipZ, signFlipXZ } from "./fsw-sign-flip.js";
-import { signGenerateTemporalIdx } from "./fsw-sign-generate-sequence.js";
-import { symbolDescribe } from "./fsw-symbol-description.js";
-import { symbolSwapHands, symbolSwapPerspective, symbolSwapSides, symbolMirror } from "./fsw-symbol-swap.js";
 import { isLeftHand, isRightHand, isBothHand, isFloorPlane, isWallPlane, isDiagonalAway, isDiagonalTowards, isDiagonalPlane, getHandOrientation } from "./fsw-symbol-variant.js";
+import { symbolSwapHands, symbolSwapPerspective, symbolSwapSides, symbolMirror } from "./fsw-symbol-swap.js";
+import { symbolDescribe } from "./fsw-symbol-description.js";
+import { signDescribe } from "./fsw-sign-description.js";
+import { signGenerateTemporalIdx } from "./fsw-sign-generate-sequence.js";
+import { signFlipX, signFlipZ, signFlipXZ } from "./fsw-sign-flip.js";
 
-export { signDescribe as describeSign, signFlipX, signFlipXZ, signFlipZ, signGenerateTemporalIdx as generateTemporalIdx, symbolDescribe as describeSymbol, symbolSwapHands, symbolSwapPerspective, symbolSwapSides, symbolMirror, isLeftHand, isRightHand, isBothHand, isFloorPlane, isWallPlane, isDiagonalAway, isDiagonalTowards, isDiagonalPlane, getHandOrientation };
+export { isLeftHand, isRightHand, isBothHand, isFloorPlane, isWallPlane, isDiagonalAway, isDiagonalTowards, isDiagonalPlane, getHandOrientation, symbolSwapHands, symbolSwapPerspective, symbolSwapSides, symbolMirror, symbolDescribe, signDescribe, signGenerateTemporalIdx, signFlipX, signFlipZ, signFlipXZ };
